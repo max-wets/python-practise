ce750c336b67bdbfb07f86ad7a2d04a8a0ecc4c4
Maxime Wets <wetsmaxime@gmail.com>
2022-12-21 22:01:52 +0100

    time calculator: small corrections

--- projects/time_calculator/time_calculator.py
+++ projects/time_calculator/time_calculator.py
@@ -15,18 +15,20 @@ def add_time(current_time, added_time, day_of_week = None):
     # number of days passed
     days_elapsed = tot_hours // 24
     tot_hours = tot_hours % 24
-    res_daytime = "AM"
+    res_daytime = "AM" 
     if (tot_hours >= 12):
         res_daytime = "PM"
         tot_hours = tot_hours - 12
+    if (tot_hours == 0):
+        tot_hours = 12
 
     week_days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
     if (day_of_week != None):
         day_of_week = str(day_of_week).casefold()
         idx = week_days.index(day_of_week)
-        end_idx = idx + days_elapsed % 7
+        end_idx = (idx + days_elapsed) % 7
         end_day = week_days[end_idx].capitalize()
     
     hours_str = '{0:02d}:{1:02d} {2}'.format(tot_hours, tot_min, res_daytime)
     days_str = f'({"next day" if days_elapsed == 1 else str(days_elapsed) + " days later"})' if days_elapsed > 0 else ''
-    return f'{hours_str}{", " + end_day if "end_day" in locals() else ""} {days_str}'
+    return f'{hours_str}{", " + end_day if "end_day" in locals() else ""} {days_str}'.strip()
